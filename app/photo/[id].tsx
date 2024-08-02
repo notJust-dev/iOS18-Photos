0747d22ef46499a2fe2930c3aad57546dfb31e26
Vadim Savin <savinvadim1312@gmail.com>
2024-08-02 18:56:58 +0300

    Go back when zooming out

--- app/photo/[id].tsx
+++ app/photo/[id].tsx
@@ -1,7 +1,8 @@
 import { Text, Image } from 'react-native';
 import { photos } from '../../data';
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import Animated, {
+  runOnJS,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
@@ -11,6 +12,7 @@ import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 export default function PhotoScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
+  const router = useRouter();
 
   const photo = photos.find((p) => p.id === Number.parseInt(id));
 
@@ -23,9 +25,14 @@ export default function PhotoScreen() {
   const gesture = Gesture.Pinch()
     .onChange((e) => {
       scale.value = e.scale;
-      console.log(e.focalX, e.focalY);
     })
-    .onEnd(() => (scale.value = withTiming(1)));
+    .onEnd((e) => {
+      if (e.velocity < 0) {
+        runOnJS(router.back)();
+      } else {
+        scale.value = withTiming(1);
+      }
+    });
 
   if (!photo) {
     return <Text>Photo not found</Text>;

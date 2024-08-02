0f7351a65f66410f49e42d2257822ff75e8cd845
Vadim Savin <savinvadim1312@gmail.com>
2024-08-02 17:12:37 +0300

    Carousel

--- App.tsx
+++ App.tsx
@@ -1,25 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  ScrollView,
+  useWindowDimensions,
+} from 'react-native';
 import { photos } from './data';
+import Carousel from './Carousel';
 
 export default function App() {
+  const { height, width } = useWindowDimensions();
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={photos}
-        numColumns={4}
-        contentContainerStyle={{ gap: 2 }}
-        columnWrapperStyle={{ gap: 2 }}
-        renderItem={({ item }) => (
-          <Image
-            source={item.image}
-            style={{ width: `${100 / 4}%`, aspectRatio: 1 }}
-          />
-        )}
-      />
+    <ScrollView style={styles.container}>
+      {/* Header */}
+      <View style={{ height: height / 2 }}>
+        <FlatList
+          data={photos}
+          numColumns={4}
+          contentContainerStyle={{ gap: 2 }}
+          columnWrapperStyle={{ gap: 2 }}
+          scrollEnabled={false}
+          inverted
+          renderItem={({ item }) => (
+            <Image
+              source={item.image}
+              style={{ width: `${100 / 4}%`, aspectRatio: 1 }}
+            />
+          )}
+        />
+      </View>
+
+      <Carousel title="Albums" photos={photos.slice(0, 6)} />
+      <Carousel title="People" photos={photos.slice(3, 6)} />
+      <Carousel title="Featured" photos={photos.slice(6, 10)} />
 
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
---
+++ Carousel.tsx
@@ -0,0 +1,49 @@
+import { View, Text, ScrollView, Image, StyleSheet } from 'react-native';
+import { photos } from './data';
+
+type Carousel = {
+  title: string;
+  photos: any[];
+};
+
+export default function Carousel({ title, photos }: Carousel) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+
+      <ScrollView
+        horizontal
+        contentContainerStyle={styles.images}
+        showsHorizontalScrollIndicator={false}
+        snapToAlignment="start"
+        snapToInterval={250 + 15}
+        decelerationRate="fast"
+      >
+        {photos.map((photo) => (
+          <Image key={photo.id} source={photo.image} style={styles.image} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    // padding: 50,
+    marginVertical: 10,
+  },
+  title: {
+    padding: 15,
+    fontWeight: '700',
+    fontSize: 20,
+  },
+  images: {
+    gap: 15,
+    paddingHorizontal: 20,
+  },
+  image: {
+    width: 250,
+    height: 150,
+    borderRadius: 15,
+  },
+});
